4dc9d0885b028207e1a823fdafc6b37c626c9206
pavroo <pavroo@onet.eu>
2022-08-12 17:48:19 +0200

    20220812

--- bullseye/stage0/00-configure-apt/01-run.sh
+++ bullseye/stage0/00-configure-apt/01-run.sh
@@ -2,12 +2,13 @@
 
 install -m 644 files/sparky.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 #install -m 644 files/sparky ${ROOTFS_DIR}"/etc/apt/preferences.d/"
+install -m 644 files/sparky.gpg.key "${ROOTFS_DIR}/usr/share/keyrings/"
 
-on_chroot apt-key add - < files/sparky.gpg.key
+# on_chroot apt-key add - < files/sparky.gpg.key
 # on_chroot apt-key add - < files/sparkylinux.gpg.key
 on_chroot << EOF
 apt-get update
 apt-get install sparky-keyring -y --allow-unauthenticated
-apt-get install sparky-apt -y --allow-unauthenticated
+apt-get install sparky-apt --no-install-recommends -y --allow-unauthenticated
 apt-get update
 EOF
--- bullseye/stage2/01-sys-tweaks/skel/.config/openbox/autostart.sh
+++ bullseye/stage2/01-sys-tweaks/skel/.config/openbox/autostart.sh
@@ -3,15 +3,21 @@
 if [ -f /usr/bin/sparky-polkit ]; then
 sparky-polkit &
 fi
-nitrogen --restore && \
+if [ -f /usr/bin/nitrogen ]; then
+nitrogen --restore &
+fi
 if [ -f /usr/bin/tint2 ]; then
 tint2 &
 elif [ -f /usr/bin/fbpanel ]; then
 fbpanel &
 fi
+if [ -f /usr/bin/xscreensaver ]; then
 xscreensaver -nosplash &
+fi
 #(sleep 10; volumeicon) &
+if [ -f /usr/bin/pnmixer ]; then
 (sleep 5; pnmixer) &
+fi
 if [ -f /opt/sparky/nm-applet-reload ]; then
 /opt/sparky/nm-applet-reload &
 fi
